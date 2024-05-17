807c5c540c878d3a2d56c4b8f862b2e8c9879d6e
MrMelon54 <github@mrmelon54.com>
2024-05-17 18:22:39 +0100

    Separate userpatch and clientstore

--- database/userpatch.go
+++ database/userpatch.go
@@ -3,7 +3,6 @@ package database
 import (
 	"fmt"
 	"github.com/1f349/tulip/database/types"
-	"github.com/go-oauth2/oauth2/v4"
 	"github.com/hardfinhq/go-date"
 	"github.com/mrmelon54/pronouns"
 	"golang.org/x/text/language"
@@ -61,22 +60,3 @@ func (u *UserPatch) ParseFromForm(v url.Values) (safeErrs []error) {
 	}
 	return
 }
-
-var _ oauth2.ClientInfo = &ClientStore{}
-
-func (c *ClientStore) GetID() string     { return c.Subject }
-func (c *ClientStore) GetSecret() string { return c.Secret }
-func (c *ClientStore) GetDomain() string { return c.Domain }
-func (c *ClientStore) IsPublic() bool    { return c.Public }
-func (c *ClientStore) GetUserID() string { return c.Owner }
-
-// GetName is an extra field for the oauth handler to display the application
-// name
-func (c *ClientStore) GetName() string { return c.Name }
-
-// IsSSO is an extra field for the oauth handler to skip the user input stage
-// this is for trusted applications to get permissions without asking the user
-func (c *ClientStore) IsSSO() bool { return c.Sso }
-
-// IsActive is an extra field for the app manager to get the active state
-func (c *ClientStore) IsActive() bool { return c.Active }
